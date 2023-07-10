d55f7cdd656e1c4f4714b038d21115c93d7788c4
Tomislav Dukez <tomdu3@ymail.com>
2023-07-10 14:10:41 +0000

    fix correct django project name

--- django_todo/asgi.py
+++ django_todo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djanago_todo project.
+ASGI config for django_todo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djanago_todo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_todo.settings')
 
 application = get_asgi_application()
